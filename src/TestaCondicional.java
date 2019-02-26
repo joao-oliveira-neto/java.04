7ca865072dd298980bb76ced52bfd7b61b36dba4
JOÃO DE OLIVEIRA NETO <JO.NETO@csi.aneas.msft>
2019-02-26 20:20:36 -0300

    java.04

---
+++ src/TestaCondicional.java
@@ -0,0 +1,16 @@
+
+public class TestaCondicional {
+
+	public static void main(String[] args) {
+		System.out.println("testando condicional");
+		int idade =17;
+		int acompanhantes = 2;
+		if (idade>= 18 || acompanhantes >=3 ) {
+			System.out.println("Voce pode entrar");
+		}
+		else {
+			System.out.print("Não pode entrar");
+		}
+	}
+
+}
---
+++ src/TestaWhile.java
@@ -0,0 +1,13 @@
+
+public class TestaWhile {
+
+	public static void main(String[] args) {
+		int contador = 1;
+		while (contador <= 5) {
+			System.out.println("oi");
+			contador++;
+		}
+
+	}
+
+}
