f1497f9d523f2b3c6e00cb13ab81adcecad83ee3
joao-oliveira-neto <joaodabete@gmail.com>
2019-03-15 13:38:02 -0300

    maiormenor

---
+++ dia 13/MaiorMenor.java
@@ -0,0 +1,30 @@
+
+
+public class MaiorMenor {
+
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		int c = 10;
+	if (a>b && a>c) {
+		System.out.println("3 é maior");
+	}
+	if (a<b && a<c) {
+		System.out.println("3 è menor");
+	}
+	if (b>a && b>c) {
+		System.out.println("5 é maior");
+	}
+	if (b<a && b<c) {
+		System.out.println("5 é menor");
+	}
+	if (c>a && c>b) {
+        System.out.println("10 é maior");
+	}
+	if (c<a && c<b) {
+		System.out.println("10 é menor");
+	}
+	
+	}
+
+}
